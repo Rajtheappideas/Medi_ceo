db0eebbfaa5bc9e69948dae7f7fd1335c445dbc4
Raj Chavda <raj.theappideas@gmail.com>
2023-07-17 19:17:54 +0530

    new set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { lazy } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "./components/ErrorFallBack";
+import Lottie from "lottie-react";
+import React, { Suspense } from "react";
+import { Toaster } from "react-hot-toast";
+import PrivateRoute from "./pages/PrivateRoute";
 
 function App() {
+  const Home = lazy(() => import("./pages/Home"));
+  const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <BrowserRouter>
+      <Toaster toastOptions={{ duration: 4000 }} position="top-center" />
+      <ErrorBoundary
+        FallbackComponent={ErrorFallback}
+        onReset={() => {
+          window.location.reload();
+        }}
+      >
+        <Suspense
+          fallback={
+            <div className="relative top-0 left-0 w-screen h-screen">
+              {/* <Lottie
+                style={{
+                  pointerEvents: "none",
+                  height: "300px",
+                  width: "300px",
+                }}
+                className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-screen h-full"
+                animationData={loading}
+                loop
+              /> */}
+              Loading...
+            </div>
+          }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <PrivateRoute>
+                  <Home />
+                </PrivateRoute>
+              }
+              caseSensitive
+            />
+
+            <Route path="/*" element={<PageNotFound />} />
+          </Routes>
+        </Suspense>
+      </ErrorBoundary>
+    </BrowserRouter>
   );
 }
 
